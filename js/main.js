e9e2134d7b762bdd6c875804768bd27c0602bad1
sehyeon0202 <soqhd0202@naver.com>
2019-03-14 22:28:24 +0900

    second commit

--- js/main.js
+++ js/main.js
@@ -9,12 +9,55 @@ $(document).ready(function(){
     });
 
 
+    //탭메뉴 animation
+
+    $('.navi>ul>li').on('click',function(e){
+            e.preventDefault();
+
+            var targetIndex = $(this).index();
+            var currentIndex = $('.slider>ul>li.on').index();
+            var isOpen = true;
+
+            
+                if(targetIndex == currentIndex){
+                    return;
+                }else{
+                sliderNext(targetIndex);
+                }
+                
+
+    })//click
+
+    function sliderNext(index){
+       
+        $('.slider>ul>li.on').stop().animate({'left':'-100%'},500,function(){
+            $(this).removeClass('on')
+        });
+
+        $('.slider>ul>li').eq(index).addClass('on').css({'left':'100%'}).stop().animate({'left':'0%'},500,function(){
+           
+        })
+
+        activeNavi(index);
+        motionTab(index);
+
+    }
 
 
+    function motionTab(index){
+        $('.tab>ul>li.on').removeClass('on');
+        $('.tab>ul>li').eq(index).addClass('on');
 
+    }
 
+    function activeNavi(index){
+        $('.navi>ul>li.on').removeClass('on');
+        $('.navi>ul>li').eq(index).addClass('on');
+       
 
-    
+    }
+
+   
 
 });
 
